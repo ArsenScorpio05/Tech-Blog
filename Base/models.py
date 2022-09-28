ab36b65d29714b57c75a8ee1350ed61a439c4084
Arsen Lufo <84084565+ArsenScorpio05@users.noreply.github.com>
2022-09-28 18:17:13 +0200

    Added Rich Text

--- Base/models.py
+++ Base/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from ckeditor.fields import RichTextField
 # Create your models here.
 
 class Topic(models.Model):
@@ -12,13 +12,13 @@ class Topic(models.Model):
 class Blog(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     title = models.TextField(max_length=200, null=True, blank=True)
-    article = models.TextField(null=True, blank=True)
+    article = RichTextField(null=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)    
     
     
     def __str__(self):
-        return self.title + " - " + self.article [0:50] + " ..."
+        return f'{self.title [0:50]} - created on: {str(self.created)}'
 
     class Meta:
         ordering = ['-created']
